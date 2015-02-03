dbddfa1eda47d6a664b3fbc87a0b25edf5f5cce3
chickenwilful <hoangyenng060493@gmail.com>
2015-02-03 16:45:44 +0800

    Add todo

---
+++ todo.py
@@ -0,0 +1,6 @@
+#TODO: post delete
+#TODO: Models + View: Vote, Category
+#TODO: Models + View: VnoiUser, VojUser
+#TODO: Manage Permission
+#TODO: Testing + Fixtures --> create dump data
+#TODO: Front-end design
