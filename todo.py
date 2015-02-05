b527a621a82b04e5acc4b5d9cf32c400cbf235b1
chickenwilful <hoangyenng060493@gmail.com>
2015-02-05 22:22:33 +0800

    update todo

--- todo.py
+++ todo.py
@@ -1,6 +1,7 @@
-#TODO: post delete
-#TODO: Models + View: Vote, Category
-#TODO: Models + View: VnoiUser, VojUser
-#TODO: Manage Permission
-#TODO: Testing + Fixtures --> create dump data
-#TODO: Front-end design
+# TODO: post delete
+# TODO: Models + View: Vote, Category
+# TODO: Models + View: VnoiUser, VojUser
+# TODO: Manage Permission
+# TODO: Testing + Fixtures --> create dump data
+# TODO: Front-end design
+# TODO: login redirect url to previous page
