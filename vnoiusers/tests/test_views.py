7f502ecd4e2c41f1e6b300509a66259c1243b3a2
tpakhoa1996 <m48554517@gmail.com>
2015-03-11 00:54:15 +0700

    user_profile test

--- vnoiusers/tests/test_views.py
+++ vnoiusers/tests/test_views.py
@@ -1,27 +1,36 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client, RequestFactory
+from django.contrib.auth.models import User
 from vnoiusers import views
 
 # ===============================user_profile==============
 
-def create_user(username, password):
-	return User.objects.create(username = username, password = password)
+class UserProfileTest(TestCase):
 
-def update_user_info(user_id, firstname, email):
-	user = User.objects.get(pk = user_id)
-	user.firstname = firstname
-	user.email = email
+    fixtures = ['forum.json', 'auth.json']
 
-class UserViewTest(TestCase):
-
-	fixtures = ['forum.json', 'auth.json']
-
-	def setUp(self):
+    def setUp(self):
         self.client = Client()
         self.factory = RequestFactory()
 
-	def test_display_profile_wrongID(self):
-		count = User.objects.count()
-		response = self.client.get(reverse('vnoiusers:user_profile', kwargs={'user_id':0}))
-		self.assertEqual(response.status_code, 200)
+    def test_user_profile(self):
+        user = User.objects.create(username = "khoa", password = "khoa", first_name = "test", email = "test@test.vn")
+
+        response = self.client.get(reverse('user:profile', kwargs={'user_id':user.pk}))
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, 'False')
+        self.assertContains(response, 'test@test.vn')
+        self.assertContains(response, 'test')
+
+        self.client.login(username = "admin", password = "admin")
+        response = self.client.get(reverse('user:profile', kwargs={'user_id':1}))
+        self.assertContains(response, True)
+
+        response = self.client.get(reverse('user:profile', kwargs={'user_id':user.pk}))
+        self.assertContains(response, 'False')
+
+        count = User.objects.count()
+        response = self.client.get(reverse('user:profile', kwargs={'user_id':(count+1)}))
+        self.assertEqual(response.status_code, 404)
+        
