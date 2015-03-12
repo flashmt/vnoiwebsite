a73b282901ab9eb89a403fd776e8ecaf231d077e
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-13 01:13:00 +0800

    Update bbcode [code] tag

--- init_database.sh
+++ init_database.sh
@@ -3,3 +3,4 @@ python manage.py migrate
 python manage.py loaddata auth.json
 python manage.py loaddata forum.json
 python manage.py loaddata pinned_topics.json
+python manage.py loaddata bbcode.json
