f548d16d13d69d162481b62db9991869ccc49d8e
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-14 03:02:43 +0800

    Implement UI for problem list

--- problems/views.py
+++ problems/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from problems.models import SpojProblem
 
 
 def index(request):
-    return render(request, 'problems/problem_list.html')
+    problems = SpojProblem.objects.all()
+    return render(request, 'problems/problem_list.html', {'problems': problems})
