2eab125e51dcb2c32fff52d82e3cb0c63ec691c8
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-14 01:13:18 +0800

    Create problems app

--- vnoiwebsite/urls.py
+++ vnoiwebsite/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^main/', include('main.urls', namespace="main")),
-    url(r'^forum/', include('forum.urls', namespace="forum")),
-    url(r'^user/', include('vnoiusers.urls', namespace="user")),
-    url(r'^message/', include('vnoimessages.urls', namespace="message")),
+    url(r'^main/', include('main.urls', namespace='main')),
+    url(r'^forum/', include('forum.urls', namespace='forum')),
+    url(r'^user/', include('vnoiusers.urls', namespace='user')),
+    url(r'^message/', include('vnoimessages.urls', namespace='message')),
     url(r'^postman/', include('postman.urls')),
+    url(r'^problem/', include('problems.urls', namespace='problems')),
 )
