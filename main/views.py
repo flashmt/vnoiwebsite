b5ce38af401fcc66e4142fb51979bf945ebc4467
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-16 02:39:24 +0800

    Implement recent posts in home page

--- main/views.py
+++ main/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from forum.models import PinnedTopic
+from forum.models import PinnedTopic, Post
 
 # Create your views here.
 
 
 def index(request):
-    pinned_posts = PinnedTopic.objects.all()
-    results = []
-    for topic in pinned_posts:
-        results.append({
+    pinned_topics = []
+    for topic in PinnedTopic.objects.all():
+        pinned_topics.append({
             'title': topic.post.title,
             'forum_id': topic.post.topic.forum_id,
             'topic_id': topic.post.topic_id,
@@ -17,6 +16,20 @@ def index(request):
             'total_vote': topic.post.total_votes(),
             'post_id': topic.post_id,
         })
+    recent_posts = []
+    posts = Post.objects.order_by('-created_at').values(
+        'pk', 'created_by__username', 'topic__title', 'topic__id', 'topic__forum__id')[:5]
+    for post in posts:
+        print post
+        recent_posts.append({
+            'post_id': post['pk'],
+            'author': post['created_by__username'],
+            'topic_title': post['topic__title'],
+            'topic_id': post['topic__id'],
+            'forum_id': post['topic__forum__id'],
+        })
+    print "recent_posts.len = %s" % len(recent_posts)
     return render(request, 'main/home.html', {
-        'pinned_topics': results
+        'pinned_topics': pinned_topics,
+        'recent_posts': recent_posts,
     })
