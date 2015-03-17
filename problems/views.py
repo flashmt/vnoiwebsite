792950a4b0e4454cf764866c526967ce1b7de9c0
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-17 21:16:03 +0800

    Optimize queries for problem list page

--- problems/views.py
+++ problems/views.py
@@ -3,5 +3,12 @@ from problems.models import SpojProblem
 
 
 def index(request):
-    problems = SpojProblem.objects.all()
+    problems = SpojProblem.objects.all().values(
+        'code',
+        'name',
+        'category__name',
+        'accept_count',
+        'score',
+    )
+    print problems[0]
     return render(request, 'problems/problem_list.html', {'problems': problems})
