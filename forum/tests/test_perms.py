dbb43e5a1569513900b1fdca5687a29be1852372
chickenwilful <hoangyenng060493@gmail.com>
2015-03-19 17:51:08 +0800

    add forum permission test

--- forum/tests/test_perms.py
+++ forum/tests/test_perms.py
@@ -3,20 +3,26 @@ from django.test import TestCase
 
 # Create your tests here.
 from forum.models import Forum, Topic, Post
+from forum.perms import PostPermission
 
 
 class PostPermissionTest(TestCase):
 
-    fixtures = ['auth.json', 'forum.json']
+    fixtures = ['auth.json', 'forum.json', 'vnoiusers.json']
 
     def setUp(self):
         self.admin = User.objects.get(username="admin")
+        self.vnoiuser = User.objects.get(username="vnoiuser")
+        self.admin_post = Post.objects.filter(created_by_id=1)[0]
+        self.vnoiuser_post = Post.objects.filter(created_by_id=2)[0]
 
     def test_post_create_permission(self):
         # Test all authenticated user can create post
-        pass
+        self.assertTrue(PostPermission(self.admin).can_create_post())
+        self.assertTrue(PostPermission(self.admin).can_create_post)
 
     def test_post_update_permission(self):
-        # test only author can update post
-        pass
-
+        # test admin or author can update post
+        self.assertTrue(PostPermission(self.admin).can_update_post(self.vnoiuser_post))
+        self.assertTrue(PostPermission(self.admin).can_update_post(self.admin_post))
+        self.assertFalse(PostPermission(self.vnoiuser).can_update_post(self.admin_post))
