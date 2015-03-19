a809f690aa78f970d9a0d36c64909cc2196ac994
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-19 23:27:55 +0800

    Refactor pinned topic model

--- main/views.py
+++ main/views.py
@@ -1,35 +1,15 @@
-from datetime import timedelta
 from django.shortcuts import render
-from django.utils import timezone
 from forum.models import PinnedTopic, Post
 
 # Create your views here.
 
 
 def index(request):
-    pinned_topics = PinnedTopic.objects.all()
-    for pinned_topic in pinned_topics:
-        if not pinned_topic.is_cached:
-            post = pinned_topic.post
-
-            pinned_topic.is_cached = True
-            pinned_topic.topic_title = post.topic.title
-            pinned_topic.forum_id = post.topic.forum_id
-            pinned_topic.topic_id = post.topic_id
-            pinned_topic.author = post.created_by.username
-            pinned_topic.content = post.content
-            pinned_topic.last_updated = timezone.now()
-            pinned_topic.total_vote = pinned_topic.post.total_votes()
-            pinned_topic.save()
-
-        if pinned_topic.last_updated < timezone.now() - timedelta(minutes=30):
-            pinned_topic.total_vote = pinned_topic.post.total_votes()
-            pinned_topic.last_updated = timezone.now()
-            pinned_topic.save()
+    pinned_topics = PinnedTopic.update_and_return_all()
 
     posts = Post.objects.order_by('-created_at').values(
         'pk', 'created_by__username', 'topic__title', 'topic__id', 'topic__forum__id')[:5]
     return render(request, 'main/home.html', {
-        'pinned_topics': PinnedTopic.objects.all(),
+        'pinned_topics': pinned_topics,
         'recent_posts': posts,
     })
