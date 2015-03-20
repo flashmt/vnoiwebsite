2ce55d06fd130af48c1b67d8d59bd995f3d6f0f6
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-20 16:01:52 +0800

    Crawl contest list from Codeforces

--- init_database.sh
+++ init_database.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Clear old database
 rm -f db.sqlite3
+
+# Create new database
 python manage.py migrate
 python manage.py loaddata auth.json
 python manage.py loaddata forum.json
@@ -9,4 +12,6 @@ python manage.py loaddata postman.json
 python manage.py loaddata mailer.json
 python manage.py loaddata problems.json
 python manage.py loaddata vnoiusers.json
-python manage.py loaddata externaljudges.json
+
+# Crawl data from external sources
+./crawl_external_judge_data.sh
