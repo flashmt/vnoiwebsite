90dfa82e3609627a44700ca4805ce7834a6d0ec0
Minh Bao <tmbao@apcs.vn>
2015-03-24 20:25:11 +0700

    Implement get_problem_statement (Not tested yet)

--- problems/crawler/crawlers.py
+++ problems/crawler/crawlers.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import django
+from django.conf import settings
 import re
 import requests
 import sys
 from bs4 import BeautifulSoup
 from problems.crawler.models import Problem
+from problems.models import SpojProblem
 
 PROBLEM_RE = re.compile(
     r'<tr class="problemrow">'
@@ -45,13 +47,40 @@ def get_problem_url(problem_code):
     return '%sproblems/%s' % (VOJ_BASE_URL, problem_code)
 
 
-def get_problems_from_category(category):
+def get_problem_statement(problem_code):
+    problem_url = "%sproblems/%s" % (VOJ_BASE_URL, problem_code)
+    soup = get_html(problem_url)
+
+    # problem statement
+    prob_content = soup.find("div", {"class": "prob"})
+    # gg+ ads
+    div_gg_ads = soup.find("div", {"style": "position: absolute; right: 0px"})
+    # fb ads
+    div_fb_ads = soup.find("div", {"class": "aProblemTop"})
+    # problem info
+    tab_prob_info = soup.find("table", {"class": "probleminfo"})
+    # comments
+    ccontent = soup.find("div", {"id": "ccontent"})
+
+    prob_statement = prob_content.text
+
+    # remove redundant sections
+    prob_statement.replace(div_fb_ads.text, "")
+    prob_statement.replace(div_gg_ads.text, "")
+    prob_statement.replace(tab_prob_info.text, "")
+    prob_statement.replace(ccontent, "")
+
+    return prob_statement
+
+
+def get_problem_codes_from_category(category):
     result = []
 
     page_id = 0
     while True:
 
-        if page_id >= 2:
+        # For testing
+        if page_id >= 1:
             break
 
         problem_rows = get_elements_from_url(get_category_url(page_id=page_id), 'tr[class="problemrow"]')
@@ -66,20 +95,21 @@ def get_problems_from_category(category):
             if matcher:
                 data = matcher.groupdict()
                 problem_code = data['code']
+                problem = Problem(problem_code)
+                problem.fields['category'] = category.pk
+                problem.fields['problem_id'] = data['id']
+                problem.fields['name'] = data['name'].replace('ð', 'đ')
+                problem.fields['statement'] = get_problem_statement(problem_code)
 
-                
+                if category.fields['name'] == 'acm':
+                    problem.fields['accept_count'] = data['ac_count']
+                    problem.fields['accept_rate'] = data['ac_rate']
+                    problem.fields['score'] = round(80.0 / (40 + int(data['ac_count'])), 1)
 
-                # problem = Problem(problem_code)
-                # problem.fields['category'] = category.pk
-                # problem.fields['problem_id'] = data['id']
-                # problem.fields['name'] = data['name'].replace('ð', 'đ')
-
-                # if category.fields['name'] == 'acm':
-                #     problem.fields['accept_count'] = data['ac_count']
-                #     problem.fields['accept_rate'] = data['ac_rate']
-                #     problem.fields['score'] = round(80.0 / (40 + int(data['ac_count'])), 1)
-
-                # result.append(problem)
+                result.append(problem)
+                if problem_code == 'CTAIN':
+                    print data
+                    print problem.fields
 
         page_id += 1
 
