0ad0615319306ddc501f032704996e6cd1e316f1
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-26 01:58:16 +0800

    Update crawler

--- problems/crawler/main.py
+++ problems/crawler/main.py
@@ -1,17 +1,32 @@
-from problems.crawler.crawlers import get_problems_from_category
-from problems.crawler.models import ProblemCategory
+import os
 
-problem_category_acm = ProblemCategory('acm')
-problem_category_oi = ProblemCategory('oi')
+# Before importing some Django lib, we must have this
+from django.core.wsgi import get_wsgi_application
 
-problems_acm = get_problems_from_category(problem_category_acm)
-problems_oi = get_problems_from_category(problem_category_oi)
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configurations.settings")
 
-
-def array_to_json(arr):
-    return '[' + ', '.join([x.to_json() for x in arr]) + ']'
+from problems.crawler.crawlers import get_problem_codes_from_category
+from problems.models import SpojProblemCategory
 
 
-print array_to_json([problem_category_acm, problem_category_oi])
-print array_to_json(problems_acm)
-print array_to_json(problems_oi)
+if __name__ == "__main__":
+    application = get_wsgi_application()
+    problem_category_acm = SpojProblemCategory.objects.filter(name='acm')
+    if len(problem_category_acm) == 0:
+        problem_category_acm = SpojProblemCategory.objects.create(name='acm')
+        problem_category_acm.save()
+    else:
+        problem_category_acm = problem_category_acm[0]
+
+    problem_category_oi = SpojProblemCategory.objects.filter(name='oi')
+    if len(problem_category_oi) == 0:
+        problem_category_oi = SpojProblemCategory.objects.create(name='oi')
+        problem_category_oi.save()
+    else:
+        problem_category_oi = problem_category_oi[0]
+
+    problems_acm = get_problem_codes_from_category(problem_category_acm)
+    problems_oi = get_problem_codes_from_category(problem_category_oi)
+
+    print [problem_category_acm, problem_category_oi]
