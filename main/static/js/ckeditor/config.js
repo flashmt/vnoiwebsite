0cd7d860309f0d5b0314087155ec81dfc9c0d1a1
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-26 19:27:23 +0800

    Implement AJAX voting in home page

--- main/static/js/ckeditor/config.js
+++ main/static/js/ckeditor/config.js
@@ -22,7 +22,6 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'colors' },
 		{ name: 'tools' },
 		{ name: 'others' },
-		{ name: 'about' }
 	];
 
 	// The default plugins included in the basic setup define some buttons that
