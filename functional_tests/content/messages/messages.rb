c653664cbb73f5238b9f94e45ba5f01136f6fe4a
tungnx94 <xtungnguyen94@gmail.com>
2015-03-31 15:52:14 +0200

    add content file (search string) for messages

---
+++ functional_tests/content/messages/messages.rb
@@ -0,0 +1,15 @@
+﻿$inbox_content = [
+  'Thư đã gửi',
+  'Hộp thư đến',
+  'Viết thư mới',
+  'Thư đã lưu',
+  'Thùng rác',
+]
+
+$reply = 'Reply'
+$edit = 'Edit'
+$messages = 'Messages'
+$messages_empty = 'Không có thư mới.'
+$messages_archived = 'Messages or conversations successfully archived.'
+$messages_deleted = 'Messages or conversations successfully deleted.'
+$messages_recovered = 'Messages or conversations successfully recovered.'
